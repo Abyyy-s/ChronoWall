7204ea8cfc4ab631f7581a80ed3a84821c21fe55
Aby S Biju <abisbiju321@gmail.com>
2026-07-16 22:03:59 +0530

    implimentedsd first header file

---
+++ include/WallpaperFrame.h
@@ -0,0 +1,19 @@
+#ifndef WALLPAPERFRAME_H
+#define WALLPAPERFRAME_H
+
+#include <string>
+
+class WallpaperFrame
+{
+private:
+    std::string imagePath;
+    int duration;
+
+public:
+    WallpaperFrame(std::string imagePath, int duration);
+
+    std::string getImagePath();
+    int getDuration();
+};
+
+#endif
