52baec97f75b259ada35457b6b5ef1998d7e2771
Aby S Biju <abisbiju321@gmail.com>
2026-07-17 22:18:06 +0530

    Implement WallpaperFrame class and CMake build system

---
+++ build/CMakeFiles/ChronoWall.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChronoWall.dir/src/WallpaperFrame.cpp.o"
+  "CMakeFiles/ChronoWall.dir/src/WallpaperFrame.cpp.o.d"
+  "CMakeFiles/ChronoWall.dir/src/main.cpp.o"
+  "CMakeFiles/ChronoWall.dir/src/main.cpp.o.d"
+  "ChronoWall"
+  "ChronoWall.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChronoWall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
