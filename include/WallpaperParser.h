868664f8761ed6110c93765da5041d3539ad1988
Aby S Biju <abisbiju321@gmail.com>
2026-07-17 22:46:14 +0530

    create the class for xml parser

---
+++ include/WallpaperParser.h
@@ -0,0 +1,17 @@
+
+
+#ifndef WALLPAPERPARSER_H
+#define WALLPAPERPARSER_H
+
+#include <string>
+#include <vector>
+
+#include "WallpaperFrame.h"
+
+class WallpaperParser
+{
+public:
+    std::vector<WallpaperFrame> parse(std::string xmlPath);
+};
+
+#endif
