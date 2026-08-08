8510acb373cf318298f3af9eba045dd6b241ca42
Aby S Biju <abisbiju321@gmail.com>
2026-08-08 21:24:13 +0530

    Refactor transition renderer to persistent GPU renderer

--- include/TransitionRenderer.h
+++ include/TransitionRenderer.h
@@ -1,12 +1,18 @@
 #ifndef TRANSITIONRENDERER_H
 #define TRANSITIONRENDERER_H
 
+#include "IDesktopSurface.h"
 #include "Transition.h"
 
 class TransitionRenderer
 {
 public:
-    bool render(const Transition &transition) const;
+    bool init(IDesktopSurface &surface);
+    bool render(const Transition &transition);
+    void shutdown();
+
+private:
+    SDL_Renderer *renderer = nullptr;
 };
 
 #endif
