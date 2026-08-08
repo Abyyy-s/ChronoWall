9f6f5586b839e361a28470304eafc8c1c099e63c
Aby S Biju <abisbiju321@gmail.com>
2026-08-08 19:36:18 +0530

    Add initial TransitionRenderer interface

---
+++ include/TransitionRenderer.h
@@ -0,0 +1,12 @@
+#ifndef TRANSITIONRENDERER_H
+#define TRANSITIONRENDERER_H
+
+#include "Transition.h"
+
+class TransitionRenderer
+{
+public:
+    bool render(const Transition &transition) const;
+};
+
+#endif
